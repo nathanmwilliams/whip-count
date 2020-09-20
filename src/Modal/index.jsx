d07cca5f6a750f27338e0b65052b67bc2a6bde39
meganrm <meganrm@gmail.com>
2020-09-20 16:33:10 -0700

    styling modal

--- src/Modal/index.jsx
+++ src/Modal/index.jsx
@@ -1,64 +1,118 @@
 import React from "react";
-import { Modal, Card } from "antd";
+import { Modal, Card, Row, Col, List } from "antd";
 import {
   FacebookOutlined,
   TwitterOutlined,
   LinkOutlined,
 } from "@ant-design/icons";
 import { STATUS_TYPES } from "../constants";
+import "./style.css";
 
 const { Meta } = Card;
 
-const SenatorModal = ({ senator, visible, closeModal }) => (
+const townHallsDisplay = (townHalls) => (
+  <Card title="Attend an event">
+    <List
+      itemLayout="vertical"
+      dataSource={townHalls}
+      renderItem={(item) => {
+          console.log(item)
+          return (
+            <List.Item
+              extra={
+                item.link
+                  ? (<a href={item.link}>{item.linkName || "More info"}</a>)
+                  : []
+              }
+  
+            >
+              <List.Item.Meta
+                title={
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`https://townhallproject.com/${item.eventId}`}
+                  >
+                    {item.eventName}
+                  </a>
+                }
+                description={`${item.meetingType} at ${item.Time} on ${item.dateString}`}
+              />
+            </List.Item>
+          );}}
+    />
+  </Card>
+);
+
+const SenatorModal = ({ senator, visible, closeModal, townHalls }) => (
   <>
     <Modal
+      width={"80%"}
       title={`Senator ${senator.displayName} (${senator.party[0]})`}
       visible={visible}
       onOk={closeModal}
       onCancel={closeModal}
+      footer={null}
     >
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <img
-            alt="example"
-            src={`https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`}
-          />
-        }
-      >
-        <Meta description={`Position: ${STATUS_TYPES[senator.status]}`} />
-      </Card>
-      <div>
-        <p className="quote">
-            {senator.quote.text}
-        </p>
-        <h4>Contact</h4>
-        <p>{senator.contact.address}</p>
-        <p>{senator.contact.phone}</p>
-        <p className="socials">
-          {senator.socials.twitter && (
-            <a target="_blank" href={`https://twitter.com/${senator.socials.twitter}`}>
-              <TwitterOutlined />
-            </a>
-          )}
+      <div className="modal-row">
+        <div className="left-container modal-col">
+          <Card
+            // style={{ maxWidth: 240 }}
+            bordered={false}
+            className={`status__${senator.status}`}
+            cover={
+              <img
+                alt="example"
+                src={`https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`}
+              />
+            }
+          >
+            <Meta description={`Position: ${STATUS_TYPES[senator.status]}`} />
+          </Card>
+        </div>
+        <div className="right-container modal-col">
+          {senator.quote && <p className="quote">{senator.quote.text}</p>}
+          <Card
+            title="Contact the senator:"
+            actions={[
+              senator.socials.facebook && (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`https://facebook.com/${senator.socials.facebook}`}
+                >
+                  <FacebookOutlined />
+                </a>
+              ),
+              senator.socials.twitter && (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`https://twitter.com/${senator.socials.twitter}`}
+                >
+                  <TwitterOutlined />
+                </a>
+              ),
+              senator.socials.url && (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={senator.socials.urls}
+                >
+                  <LinkOutlined />
+                </a>
+              ),
+            ]}
+          >
+            <p>{senator.contact.address}</p>
+            <p>{senator.contact.phone}</p>
+          </Card>
 
-          {senator.socials.facebook && (
-            <a target="_blank" href={`https://facebook.com/${senator.socials.facebook}`}>
-              <FacebookOutlined />
-            </a>
-          )}
-
-          {senator.socials.url && (
-            <a target="_blank" href={senator.socials.urls}>
-              <LinkOutlined />
-            </a>
-          )}
-        </p>
+          {townHalls.length > 0 && townHallsDisplay(townHalls)}
+        </div>
       </div>
     </Modal>
   </>
 );
-
 
 export default SenatorModal;
