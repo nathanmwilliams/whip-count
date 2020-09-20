2eeab47d2e608e99daaff10bf6b5597f36241117
meganrm <meganrm@gmail.com>
2020-09-20 01:04:45 -0700

    Updates

---
+++ precache-manifest.de5270b83b57aeca642f890c310bfee7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbc861b20cdf0110a37143be215ddb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0db63f2356e1686cc92",
+    "url": "/static/css/2.e3e7c3cb.chunk.css"
+  },
+  {
+    "revision": "3f5ad5934bbf68dc52f7",
+    "url": "/static/css/main.ff621ddb.chunk.css"
+  },
+  {
+    "revision": "a0db63f2356e1686cc92",
+    "url": "/static/js/2.658163d5.chunk.js"
+  },
+  {
+    "revision": "70398313a0ec7fecf519184b0e1f7817",
+    "url": "/static/js/2.658163d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5ad5934bbf68dc52f7",
+    "url": "/static/js/main.0c6c5c85.chunk.js"
+  },
+  {
+    "revision": "68cd3f25054f718d93ef",
+    "url": "/static/js/runtime-main.a3afca20.js"
+  }
+]);
