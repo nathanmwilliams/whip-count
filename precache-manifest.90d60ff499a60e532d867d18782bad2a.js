7c521933f31cdd15973fdfa844dc33a507831dab
meganrm <meganrm@gmail.com>
2020-09-20 01:12:25 -0700

    Updates

---
+++ precache-manifest.90d60ff499a60e532d867d18782bad2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c123e255ef807c76e6c96f7e6d325f26",
+    "url": "/whip-count/index.html"
+  },
+  {
+    "revision": "78a7926e253d3ef01bb8",
+    "url": "/whip-count/static/css/2.e3e7c3cb.chunk.css"
+  },
+  {
+    "revision": "5531e6039b9bad190af2",
+    "url": "/whip-count/static/css/main.ff621ddb.chunk.css"
+  },
+  {
+    "revision": "78a7926e253d3ef01bb8",
+    "url": "/whip-count/static/js/2.c61041ae.chunk.js"
+  },
+  {
+    "revision": "70398313a0ec7fecf519184b0e1f7817",
+    "url": "/whip-count/static/js/2.c61041ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5531e6039b9bad190af2",
+    "url": "/whip-count/static/js/main.5250f143.chunk.js"
+  },
+  {
+    "revision": "a3e3db62c3af7849bd06",
+    "url": "/whip-count/static/js/runtime-main.5c2234c8.js"
+  }
+]);
