73f504e03dcdccd536100071a3f105f556dd223e
meganrm <meganrm@gmail.com>
2020-09-20 19:03:22 -0700

    Updates

---
+++ precache-manifest.1a0d5b91000072f82b8cfa44059c0d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde46dcd091c7c18c95d798894d7f4d2",
+    "url": "/whip-count/index.html"
+  },
+  {
+    "revision": "e00c351dbccda9021259",
+    "url": "/whip-count/static/css/2.e29af080.chunk.css"
+  },
+  {
+    "revision": "6e123d508ac36019b9eb",
+    "url": "/whip-count/static/css/main.d8148546.chunk.css"
+  },
+  {
+    "revision": "e00c351dbccda9021259",
+    "url": "/whip-count/static/js/2.cb704dd3.chunk.js"
+  },
+  {
+    "revision": "29071d71a4f2bfccb793930ac64b1be2",
+    "url": "/whip-count/static/js/2.cb704dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e123d508ac36019b9eb",
+    "url": "/whip-count/static/js/main.38540127.chunk.js"
+  },
+  {
+    "revision": "a3e3db62c3af7849bd06",
+    "url": "/whip-count/static/js/runtime-main.5c2234c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whip-count/precache-manifest.0ed6da6784c5224ee4b2e5867f99d97a.js"
+  "/whip-count/precache-manifest.1a0d5b91000072f82b8cfa44059c0d6e.js"
 );
 
 self.addEventListener('message', (event) => {
